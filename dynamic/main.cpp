686e12ff04741e0802b86716354c7a192145cd35
meysam <mysm.shahbazi@gmail.com>
2022-10-23 17:49:42 +0330

    at middle of tut3

--- dynamic/main.cpp
+++ dynamic/main.cpp
@@ -1,6 +1,7 @@
 #include <gst/gst.h>
 
 // Structure to contain all our information, so we can pass it to callbacks
+
 typedef struct _CustomData
 {
     GstElement *pipeline;
@@ -11,9 +12,106 @@ typedef struct _CustomData
 
 } CustomData;
 
-
+// handler for the pad-added signal 
+static void pad_added_handler(GstElement *src,GstPad *pad, CustomData *data)
 int main(int argc, char * argv[])
-{
+{  
+    CustomData data;
+    GstBus *bus;
+    GstMessage *msg;
+    GstStateChangeReturn ret;
+    gboolean terminate = FALSE;
+
+    gst_init(&argc, &argv);
+
+
+    data.source = gst_element_factory_make("uridecodebin", "source");
+    data.convert = gst_element_factory_make("audioconvert", "convert");
+    data.resample = gst_element_factory_make("audioresample", "resample");
+    data.sink = gst_element_factory_make("autoaudiosink", "sink");
+
+    data.pipeline = gst_pipeline_new("test-pipeline");
+
+    if (!data.pipeline || !data.source || !data.convert || !data.resample || !data.sink) 
+    {
+        g_printerr ("Not all elements could be created.\n");
+        return -1;
+    }
+
+    // Build the pipeline. Note that we are NOT linking the source at this
+    // point. We will do it later.
+    gst_bin_add_many(GST_BIN(data.pipeline), data.source, data.convert, data.resample, data.sink,NULL);
+
+    if(!gst_element_link_many(data.convert, data.resample, data.sink,NULL))
+    {
+        g_printerr("Element could not be linked.\n");
+        gst_buffer_unref(data.pipeline);
+        return -1;
+    }
+
+    // Set the URI to play
+    g_object_set (data.source, "uri", "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm", NULL);
+
+    g_signal_connect(data.source, "pad-added",G_CALLBACK(pad_added_handler), &data);
+
+    ret = gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
+
+    if(ret == GST_STATE_CHANGE_FAILURE)
+    {
+        g_printerr("Unable to set the pipeline to the playing state.\n");
+        gst_object_unref(data.pipeline);
+        return -1;
+    }
+
+    bus = gst_element_get_bus(data.pipeline);
+
+    do 
+    {
+        msg = gst_bus_timed_pop_filtered(bus,GST_CLOCK_TIME_NONE,
+                    (GstMessageType) (GST_MESSAGE_ERROR | GST_MESSAGE_EOS) );
+
+        if (msg != NULL) 
+        {
+            GError *err;
+            gchar *debug_info;
+
+            switch (GST_MESSAGE_TYPE (msg)) 
+            {
+                case GST_MESSAGE_ERROR:
+                    gst_message_parse_error (msg, &err, &debug_info);
+                    g_printerr ("Error received from element %s: %s\n", GST_OBJECT_NAME (msg->src), err->message);
+                    g_printerr ("Debugging information: %s\n", debug_info ? debug_info : "none");
+                    g_clear_error (&err);
+                    g_free (debug_info);
+                    terminate = TRUE;
+                    break;
+                case GST_MESSAGE_EOS:
+                    g_print ("End-Of-Stream reached.\n");
+                    terminate = TRUE;
+                    break;
+                case GST_MESSAGE_STATE_CHANGED:
+                    /* We are only interested in state-changed messages from the pipeline */
+                    if (GST_MESSAGE_SRC (msg) == GST_OBJECT (data.pipeline)) {
+                        GstState old_state, new_state, pending_state;
+                        gst_message_parse_state_changed (msg, &old_state, &new_state, &pending_state);
+                        g_print ("Pipeline state changed from %s to %s:\n",
+                            gst_element_state_get_name (old_state), gst_element_state_get_name (new_state));
+                    }
+                    break;
+                default:
+                    /* We should not reach here */
+                    g_printerr ("Unexpected message received.\n");
+                    break;
+            }
+            gst_message_unref (msg);
+        }
+
+    }
+    while (!terminate);
+    
+    gst_object_unref (bus);
+    gst_element_set_state (data.pipeline, GST_STATE_NULL);
+    gst_object_unref (data.pipeline);
 
     return 0;
 }
