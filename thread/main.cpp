9f2e11a77812520671e139cbb20fd4f3ba2f515c
meysam <mysm.shahbazi@gmail.com>
2022-10-25 11:43:09 +0330

    work on tut7

--- thread/main.cpp
+++ thread/main.cpp
@@ -36,7 +36,29 @@ int main (int argc, char *argv[])
     g_object_set(audio_source, "freq", 215.0f, NULL);
     g_object_set(visual, "shader", 0, "style", 1, NULL);
 
-    
+    gst_bin_add_many(GST_BIN (pipeline), audio_source, tee, audio_queue, audio_convert,
+                    audio_resample, audio_sink, video_queue, visual, video_convert, video_sink, NULL);
+
+
+    if( !gst_element_link_many(audio_source, tee, NULL) ||
+        !gst_element_link_many(audio_queue, audio_convert,audio_resample,audio_sink,NULL) ||
+        !gst_element_link_many(video_queue,visual,video_convert,video_sink, NULL) )
+    {
+        g_printerr ("Elements could not be linked.\n");
+        gst_object_unref (pipeline);
+        return -1;
+    }
+
+    GstPadTemplate *gtpa;
+    tee_audio_pad = gst_element_request_pad(tee,gtpa,"src_%u",NULL);
+    g_print("Obtained request pad %s for audio branch.\n", gst_pad_get_name (tee_audio_pad));
+
+    GstPadTemplate *gtpv;
+    tee_video_pad = gst_element_request_pad(tee,gtpv,"src_%u",NULL);
+    g_print("Obtained request pad %s for video branch.\n", gst_pad_get_name (tee_video_pad));
+
+
+
 
     return 0;
 }
