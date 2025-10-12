691d56e4426b1d369767c537da4d48e381bf5746
meysam <mysm.shahbazi@gmail.com>
2025-10-12 18:04:38 +0330

    Add appsrc and appsink

--- custom-plugin/cmd.sh
+++ custom-plugin/cmd.sh
@@ -5,3 +5,26 @@
 git clone https://gitlab.freedesktop.org/gstreamer/gst-template.git
 
 
+cd gstreamer/subprojects/gst-plugins-bad/tools/
+
+./gst-element-maker rtpsync basetransform
+
+mkdir ../gst/rtpsync
+
+
+cp *.h and *.c to ../gst/rtpsync
+
+cd ../gst/rtpsync/
+
+cp ../sdp/meson.build . # and edit with suitable names 
+# also edit top level meson.build and add rtpsync
+
+ninja -C build subprojects/gst-plugins-bad/gst/libgstmycustomelement.so
+
+ninja -C build install
+
+
+rm -rf ~/.cache/gstreamer-1.0/*
+gst-inspect-1.0 rtpsync
+
+
--- sync-rtp/main.cpp
+++ sync-rtp/main.cpp
@@ -1,143 +1,172 @@
 #include <gst/gst.h>
 #include <iostream>
-// #include "my_frame_id_meta.h" 
 // #include "gst/video/gstvideometa.h"
+// #include "my_frame_id_meta.h" 
 
-#include "gstmyfilter.h"
+// #include "gstmyfilter.h"
 
 #define CUSTOM_UUID_LEN 16
 
-
-
-static GstPadProbeReturn cb_have_data(GstPad *pad, GstPadProbeInfo *info, gpointer user_data) {
-    gint x, y;
-    GstMapInfo map;
-    GstMapInfo map2;
-    guint8 *ptr;
-    guint16 *ptr2;
-    GstBuffer *buffer;
-    GstBuffer *buffer2;
-
-    GstElement *rtph264depay = (GstElement *)user_data;
-
-
-    buffer = GST_PAD_PROBE_INFO_BUFFER (info);
-    buffer = gst_buffer_make_writable (buffer);
-
-    
-
-    /* Making a buffer writable can fail (for example if it
-    * cannot be copied and is used more than once)
-    */
-
-     if (buffer == NULL)
-        return GST_PAD_PROBE_OK;
-
-    /* Mapping a buffer can fail (non-writable) */
-    if (gst_buffer_map (buffer, &map, GST_MAP_WRITE)) {
-        
-        ptr = (guint8 *) map.data;
-        for (int i =0; i < 20; i++)
-            std::cout << int(ptr[i]) << ",";
-        
-        std::cout<< std::endl;
-
-        for (int i = 5; i < map.size-1; i++) {
-            ptr[i] = ptr[i+1];
-        }
-
-        for (int i =0; i < 20; i++)
-            std::cout << int(ptr[i]) << ",";
-
-        std::cout << "\ts: " << map.size << std::endl;
-        // for (int i = 0; i < map.size - 1; i++)
-        //     ptr[i] = ptr[i];
-
-        // map.size = map.size - 1;
-        gst_buffer_unmap (buffer, &map);
-    }
-    // std::cout << "im heree \n";
-
-    GST_PAD_PROBE_INFO_DATA(info) = buffer;
-
-
-    // if (buffer == NULL)
-    //     return GST_PAD_PROBE_OK;
-
-    // /* Mapping a buffer can fail (non-writable) */
-    // if (gst_buffer_map (buffer, &map, GST_MAP_WRITE)) {
-    //     buffer2 = gst_buffer_new_and_alloc (map.size - 1);
-    //     buffer2 = gst_buffer_make_writable (buffer2);
-    //     gst_buffer_map (buffer2, &map2, GST_MAP_WRITE);
-    //     ptr = (guint16 *) map.data;
-    //     ptr2 = (guint16 *) map2.data;
-    //     std::cout << int(ptr[0]) << " s: " << map.size << " , "  << map2.size << std::endl;
-    //     for (int i = 0; i < map.size - 1; i++)
-    //         ptr2[i] = ptr[i + 1];
-
-
-    //     gst_buffer_unmap (buffer2, &map2);
-    //     gst_buffer_unmap (buffer, &map);
-    // }
-    // // std::cout << "im heree \n";
-
-    // GST_PAD_PROBE_INFO_DATA(info) = buffer2;
-
-    // gst_buffer_unref (buffer2);
-    return GST_PAD_PROBE_OK;
-}
-
 // static GstPadProbeReturn cb_have_data(GstPad *pad, GstPadProbeInfo *info, gpointer user_data) {
+//     gint x, y;
+//     GstMapInfo map;
+//     GstMapInfo map2;
+//     guint8 *ptr;
+//     guint16 *ptr2;
+//     GstBuffer *buffer;
+//     GstBuffer *buffer2;
+
 //     GstElement *rtph264depay = (GstElement *)user_data;
 
-//     GstStructure *stat;
-
-//     gint val = 0;
-    
-//     // g_object_get(rtph264depay, "stats", &stat, NULL);
-//     // gst_structure_get_int (stat,"timestamp", &val);
-//     // std::cout << val << std::endl;
-
-//     // gint x, y;
-//     // GstMapInfo map;
-//     // guint16 *ptr, t;
-//     GstBuffer *buffer;
 
 //     buffer = GST_PAD_PROBE_INFO_BUFFER (info);
+//     buffer = gst_buffer_make_writable (buffer);
 
-//     std::cout << "im here\n";
-//     GstVideoCropMeta *meta = 
-//     (GstVideoCropMeta *) gst_buffer_get_meta (buffer, GST_VIDEO_CROP_META_API_TYPE);
+//     /* Making a buffer writable can fail (for example if it
+//     * cannot be copied and is used more than once)
+//     */
+//      if (buffer == NULL)
+//         return GST_PAD_PROBE_OK;
 
-//     if (meta) {
-//         guint64 received_frame_id = meta->x;
-
-//         std::cout <<"received_frame_id " << received_frame_id << std::endl;
-//         // Use 'received_frame_id' as the key to look up the corresponding 
-//         // (x, y, w, h) data you received over the separate UDP socket.
+//     /* Mapping a buffer can fail (non-writable) */
+//     if (gst_buffer_map (buffer, &map, GST_MAP_WRITE)) {
         
-//         // Store this video frame in a map/queue indexed by 'received_frame_id'.
+//         ptr = (guint8 *) map.data;
+//         for (int i =0; i < 20; i++)
+//             std::cout << int(ptr[i]) << ",";
+        
+//         std::cout<< std::endl;
+
+//         // for (int i = 5; i < map.size-1; i++) {
+//         //     ptr[i] = ptr[i+1];
+//         // }
+
+//         for (int i =0; i < 20; i++)
+//             std::cout << int(ptr[i]) << ",";
+
+//         std::cout << "\ts: " << map.size << std::endl;
+//         // for (int i = 0; i < map.size - 1; i++)
+//         //     ptr[i] = ptr[i];
+
+//         // map.size = map.size - 1;
+//         gst_buffer_unmap (buffer, &map);
 //     }
-
-
-//     // auto meta = gst_buffer_get_custom_meta (buffer, "mymeta");
-//     // auto metadata = gst_custom_meta_get_structure (meta);
-//     // gint64 property;
-//     // gst_structure_get_int64 (metadata, "property_name", &property);
-//     // std::cout <<property << std::endl;
-
-//     // buffer = gst_buffer_make_writable (buffer);
+//     // std::cout << "im heree \n";
 
 //     GST_PAD_PROBE_INFO_DATA(info) = buffer;
-    
+
+
 //     return GST_PAD_PROBE_OK;
 // }
 
+struct CustomData {
+    guint8* ptr;
+    int size;
+
+    GstElement *appsink, *appsrc;
+    guint sourceid;
+
+};
+
+
+/* The appsink has received a buffer */
+static GstFlowReturn new_sample (GstElement *sink, CustomData *data) {
+    GstSample *sample;
+    GstFlowReturn ret;
+    /* Retrieve the buffer */
+    std::cout << "im here in new sample\n";
+    g_signal_emit_by_name (sink, "pull-sample", &sample);
+    if (sample) {
+        GstBuffer* rec_buff = gst_sample_get_buffer(sample);
+
+
+
+        // /* The only thing we do in this example is print a * to indicate a received buffer */
+        // g_print ("*");
+
+        g_signal_emit_by_name (data->appsrc, "push-buffer", rec_buff, &ret);
+        // push_data(data);
+        // gst_buffer_unref(rec_buff);
+
+        gst_sample_unref (sample);
+        return GST_FLOW_OK;
+    }
+
+  return GST_FLOW_ERROR;
+}
+
+
+static gboolean push_data(CustomData *data) {
+  GstBuffer *buffer;
+  GstFlowReturn ret;
+  int i;
+  GstMapInfo map;
+  gint16 *raw;
+//   gint num_samples = CHUNK_SIZE / 2; /* Because each sample is 16 bits */
+
+  /* Create a new empty buffer */
+  buffer = gst_buffer_new_and_alloc (data->size);
+
+  /* Set its timestamp and duration */
+//   GST_BUFFER_TIMESTAMP (buffer) = gst_util_uint64_scale (data->num_samples, GST_SECOND, SAMPLE_RATE);
+//   GST_BUFFER_DURATION (buffer) = gst_util_uint64_scale (num_samples, GST_SECOND, SAMPLE_RATE);
+
+  /* Generate some psychodelic waveforms */
+  gst_buffer_map (buffer, &map, GST_MAP_WRITE);
+  raw = (gint16 *)map.data;
+
+
+
+
+
+  /* Push the buffer into the appsrc */
+  g_signal_emit_by_name (data->appsrc, "push-buffer", buffer, &ret);
+
+  /* Free the buffer now that we are done with it */
+  gst_buffer_unref (buffer);
+
+  if (ret != GST_FLOW_OK) {
+    /* We got some error, stop sending data */
+    return FALSE;
+  }
+
+  return TRUE;
+}
+
+/* This signal callback triggers when appsrc needs data. Here, we add an idle handler
+ * to the mainloop to start pushing data into the appsrc */
+static void start_feed (GstElement *source, guint size, CustomData *data) {
+  if (data->sourceid == 0) {
+    g_print ("Start feeding\n");
+    data->sourceid = g_idle_add ((GSourceFunc) push_data, data);
+  }
+}
+
+/* This callback triggers when appsrc has enough data and we can stop sending.
+ * We remove the idle handler from the mainloop */
+static void stop_feed (GstElement *source, CustomData *data) {
+  if (data->sourceid != 0) {
+    g_print ("Stop feeding\n");
+    g_source_remove (data->sourceid);
+    data->sourceid = 0;
+  }
+}
+
+
+
 gint main (gint   argc, gchar *argv[]) {
     GMainLoop *loop;
-    GstElement *pipeline, *udpsrc, *capsfilter, *queue,*rtph264depay ,*h264parse,*nvh264dec,*videoconvert,*autovideosink;
-    GstElement* myfilter;
-    GstCaps *filtercaps;
+    GstElement*udpsrc, *capsfilter, *queue, 
+    // *appsink, *appsrc,
+    *rtph264depay,*h264parse,
+    *nvh264dec,*videoconvert,*autovideosink;
+
+    GstElement *pipeline1;
+    GstElement *pipeline2;
+    
+
+    CustomData data;
+    GstCaps *filtercaps, *caps;
     GstPad *pad;
 
     /* init GStreamer */
@@ -145,33 +174,23 @@ gint main (gint   argc, gchar *argv[]) {
     loop = g_main_loop_new (NULL, FALSE);
 
     /* build */
-    pipeline = gst_pipeline_new ("my-pipeline");
+    pipeline1 = gst_pipeline_new ("my-pipeline1");
+    pipeline2 = gst_pipeline_new ("my-pipeline2");
 
     udpsrc = gst_element_factory_make ("udpsrc", "udp_src");
-    if (udpsrc == NULL)
-        g_error ("Could not create 'videotestsrc' element");
 
-
-    myfilter = gst_element_factory_make ("my_filter", "my_filter");
-    if (myfilter == NULL)
-        g_error ("Could not create 'myfilter' element"); 
+    data.appsrc = gst_element_factory_make ("appsrc", "app_src");
+    data.appsink = gst_element_factory_make ("appsink", "app_sink");
 
     capsfilter = gst_element_factory_make ("capsfilter", "caps_filter");
     g_assert (capsfilter != NULL); /* should always exist */
 
     queue = gst_element_factory_make ("queue", "queue0");
-    if (queue == NULL)
-        g_error ("Could not create 'queue' element");
-
     rtph264depay = gst_element_factory_make ("rtph264depay", "rtph264_depay");
-    if (rtph264depay == NULL)
-        g_error ("Could not create 'rtph264depay' element");
-
     h264parse = gst_element_factory_make ("h264parse", "h264_parse");
-    if (h264parse == NULL)
-        g_error ("Could not create 'h264parse' element");
+    
 
-    nvh264dec = gst_element_factory_make ("nvh264dec", "nvh264_dec");
+    nvh264dec = gst_element_factory_make ("avdec_h264", "nvh264_dec");
     if (nvh264dec == NULL)
         g_error ("Could not create 'nvh264dec' element");
 
@@ -184,39 +203,75 @@ gint main (gint   argc, gchar *argv[]) {
         g_error ("Could not create neither 'xvimagesink' nor 'ximagesink' element");
 
 
-    gst_bin_add_many (GST_BIN (pipeline), udpsrc, capsfilter, queue, rtph264depay,myfilter, h264parse,nvh264dec,videoconvert,autovideosink, NULL);
+    gst_bin_add_many (GST_BIN (pipeline1), udpsrc, capsfilter, queue, 
+        rtph264depay,h264parse, data.appsink, NULL);
 
-    gst_element_link_many (udpsrc, capsfilter, queue, rtph264depay, h264parse,myfilter, nvh264dec,videoconvert,autovideosink, NULL);
+    gst_bin_add_many (GST_BIN (pipeline2),data.appsrc,
+        nvh264dec,videoconvert,autovideosink, NULL);
+
+    gst_element_link_many(udpsrc, capsfilter, queue, rtph264depay, h264parse, data.appsink, NULL);
+
+    gst_element_link_many(data.appsrc, nvh264dec,videoconvert,autovideosink, NULL);
+
+
+
     filtercaps = gst_caps_new_simple ("application/x-rtp",
                 "media", G_TYPE_STRING, "video",
                 "clock-rate", G_TYPE_INT, 90000,
                 "encoding-name", G_TYPE_STRING, "H264",
                 "payload", G_TYPE_INT, 96,
                 NULL);
+
+
+                
     g_object_set (G_OBJECT (capsfilter), "caps", filtercaps, NULL);
 
+
+
+    caps = gst_caps_new_simple("video/x-h264",
+                    "stream-format", G_TYPE_STRING, "byte-stream",
+                    "alignment", G_TYPE_STRING, "au",
+                    NULL);
     
     
-    gst_caps_unref (filtercaps);
+    gst_caps_unref(filtercaps);
     
-    g_object_set (G_OBJECT (udpsrc), "uri", "udp://224.1.1.3:5000", NULL);
-    g_object_set (G_OBJECT (queue), "max-size-buffers", 1, NULL);
-    g_object_set (G_OBJECT (autovideosink), "sync", FALSE, NULL);
+    g_object_set(G_OBJECT (udpsrc), "uri", "udp://224.1.1.3:5000", NULL);
+    g_object_set(G_OBJECT (queue), "max-size-buffers", 1, NULL);
+    g_object_set(G_OBJECT (autovideosink), "sync", FALSE, NULL);
+
+
+    g_object_set (data.appsrc, "caps", caps,  "format", GST_FORMAT_TIME, NULL);
+    g_signal_connect (data.appsrc, "need-data", G_CALLBACK (start_feed), &data);
+    g_signal_connect (data.appsrc, "enough-data", G_CALLBACK (stop_feed), &data);
+
+    
+    /* Configure appsink */
+    g_object_set (data.appsink, "emit-signals", TRUE, "caps", caps, NULL);
+    g_signal_connect (data.appsink, "new-sample", G_CALLBACK (new_sample), &data);
+
+    gst_caps_unref(caps);
+
+
+
+
+
     // pad = gst_element_get_static_pad (udpsrc, "src");
     // gst_pad_add_probe (pad, GST_PAD_PROBE_TYPE_BUFFER,
     //     (GstPadProbeCallback) cb_have_data, NULL, NULL);
     // gst_object_unref (pad);
 
-    pad = gst_element_get_static_pad (rtph264depay, "src");
-    gst_pad_add_probe (pad, GST_PAD_PROBE_TYPE_BUFFER,
-        (GstPadProbeCallback) cb_have_data, (gpointer)rtph264depay, NULL);
-    gst_object_unref (pad);
+    // pad = gst_element_get_static_pad (rtph264depay, "src");
+    // gst_pad_add_probe (pad, GST_PAD_PROBE_TYPE_BUFFER,
+    //     (GstPadProbeCallback) cb_have_data, (gpointer)rtph264depay, NULL);
+    // gst_object_unref (pad);
 
     /* run */
-    gst_element_set_state (pipeline, GST_STATE_PLAYING);
+    gst_element_set_state (pipeline1, GST_STATE_PLAYING);
+    gst_element_set_state (pipeline2, GST_STATE_PLAYING);
 
     /* wait until it's up and running or failed */
-    if (gst_element_get_state (pipeline, NULL, NULL, -1) == GST_STATE_CHANGE_FAILURE) {
+    if (gst_element_get_state (pipeline1, NULL, NULL, -1) == GST_STATE_CHANGE_FAILURE) {
         g_error ("Failed to go into PLAYING state");
     }
 
@@ -224,8 +279,11 @@ gint main (gint   argc, gchar *argv[]) {
     g_main_loop_run (loop);
 
     /* exit */
-    gst_element_set_state (pipeline, GST_STATE_NULL);
-    gst_object_unref (pipeline);
+    gst_element_set_state (pipeline1, GST_STATE_NULL);
+    gst_object_unref (pipeline1);
+
+    gst_element_set_state (pipeline2, GST_STATE_NULL);
+    gst_object_unref (pipeline2);
 
     return 0;
 }
